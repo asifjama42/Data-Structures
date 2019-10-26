ffaaca2e5bcca62ad85b120b8695ada8a530e718
Mr Asif <46103851+asifi4win@users.noreply.github.com>
2019-10-26 20:02:29 +0530

    Update HeightBinaryTree.java

--- Binary Tree/HeightBinaryTree.java
+++ Binary Tree/HeightBinaryTree.java
@@ -1,8 +1,10 @@
-import java.lang.Math;;
+import java.lang.Math;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class HeightBT {
 	static int height = 0;
-
+	//Recursive Approach
 	public static int height(Node root) {
 		if (root == null) {
 			return -1;
@@ -11,33 +13,63 @@ public class HeightBT {
 
 		return height;
 	}
+	
+	// Iterative Approach
+	public static int heightBT(Node root) {
+		height = 0;
+		Queue<Node> queue = new LinkedList<>();
+		if(root == null) return 0;
+		queue.add(root);
+		while(!queue.isEmpty()) {
+			Node temp;
+			int size = queue.size();
+			while(size-->0) {
+				temp = queue.poll();
+				if(temp.left!=null) {
+					queue.add(temp.left);
+				}
+				if(temp.right!=null) {
+					queue.add(temp.right);
+				}
+			}
+			height++;
+		}
+		
+		
+		
+		
+		return height;
+	}
 
 	public static void main(String[] args) {
-		Node root = new Node(15);
-		root.left = new Node(10);
-		root.right = new Node(20);
+		Node root = new Node(1); 
+		root.left = new Node(2); 
+		root.right = new Node(3); 
 
-		root.left.left = new Node(8);
-		root.left.right = new Node(12);
-		root.right.left = new Node(16);
-		root.right.right = new Node(25);
-		root.right.right.left = new Node(23);
+		root.left.left = new Node(4); 
+		root.left.right = new Node(5); 
+		root.right.left = new Node(6); 
+		root.right.right = new Node(7); 
 
-		// root.left.left.left = new Node(8);
-		// root.left.left.right = new Node(9);
-		// root.left.right.left = new Node(10);
-		// root.left.right.right = new Node(11);
-		// root.right.left.left = new Node(12);
-		// root.right.left.right = new Node(13);
-		// root.right.right.left = new Node(14);
-		// root.right.right.right = new Node(15);
+		root.left.left.left = new Node(8); 
+		root.left.left.right = new Node(9); 
+		root.left.right.left = new Node(10); 
+		root.left.right.right = new Node(11); 
+		root.right.left.left = new Node(12); 
+		root.right.left.right = new Node(13); 
+		root.right.right.left = new Node(14); 
+		root.right.right.right = new Node(15); 
+		root.right.right.right.left = new Node(16); 
+		root.right.right.right.left.right = new Node(17); 
+
+
 
 		System.out.println("First Tree");
 		PrintBTree.print2D(root);
 		;
 		System.out.println();
 		System.out.print("Height of given Tree -> ");
-		System.out.println(height(root));
+		System.out.println(heightBT(root));
 	}
 
 }
